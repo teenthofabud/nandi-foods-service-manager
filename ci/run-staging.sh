8679ec046932c40a4ff791eba37acf39b19ea790
adas <anirban.das@ionos.com>
2024-12-22 16:14:18 +0100

    fixed server port for staging environment

--- ci/run-staging.sh
+++ ci/run-staging.sh
@@ -6,4 +6,8 @@ mvn clean package -e
 
 docker build -t teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT -f Dockerfile .
 
-docker run -p 8081:8081 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker run -p 8080:8080 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+
+docker image push teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+
+docker pull docker.io/teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
