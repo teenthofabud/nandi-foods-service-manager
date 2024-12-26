60846898ee5451ea5352aa7e15ad6e0da316cb12
adas <anirban.das@ionos.com>
2024-12-26 15:57:08 +0100

    updated staging script with updated artifact version

--- ci/run-staging.sh
+++ ci/run-staging.sh
@@ -1,13 +1,13 @@
 docker rm -vf nandi-foods-service-manager
 
-docker rmi -f teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker rmi -f teenthofabud/nandi-foods-service-manager:0.0.4-SNAPSHOT
 
 mvn clean package -e
 
-docker build -t teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT -f Dockerfile .
+docker build -t teenthofabud/nandi-foods-service-manager:0.0.4-SNAPSHOT -f Dockerfile .
 
-docker run -p 8080:8080 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker run -p 8080:8080 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.4-SNAPSHOT
 
-docker image push teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker image push teenthofabud/nandi-foods-service-manager:0.0.4-SNAPSHOT
 
-docker pull docker.io/teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker pull docker.io/teenthofabud/nandi-foods-service-manager:0.0.4-SNAPSHOT
