affec9a3d7fa2d032c5cbea404aae8c456d5ee92
adas <anirban.das@ionos.com>
2024-12-27 17:11:27 +0100

    parameterized build scripts

--- ci/run-development.sh
+++ ci/run-development.sh
@@ -1,13 +1,15 @@
+export VERSION=0.1.4-RELEASE
+
 #docker rm -vf $(docker ps -aq)
 docker rm -vf nandi-foods-service-manager
 
 #docker rmi -f $(docker images -aq)
-docker rmi -f teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker rmi -f teenthofabud/nandi-foods-service-manager:${VERSION}
 
 mvn clean package -e
 
-docker build -t teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT -f Dockerfile .
+docker build -t teenthofabud/nandi-foods-service-manager:${VERSION} -f Dockerfile .
 #docker build --build-arg PROFILE=default,development -t teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT -f Dockerfile .
 
 #docker run -p 8081:8081 -d --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
-docker run -p 8081:8081 -d -e PROFILE=default,development --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.0.3-SNAPSHOT
+docker run -p 8081:8081 -d -e PROFILE=default,development --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:${VERSION}
--- ci/run-staging.sh
+++ ci/run-staging.sh
@@ -1,13 +1,21 @@
+export VERSION=0.1.4-RELEASE
+
 docker rm -vf nandi-foods-service-manager
 
-docker rmi -f teenthofabud/nandi-foods-service-manager:0.1.4-RELEASE
+docker rmi -f teenthofabud/nandi-foods-service-manager:${VERSION}
 
 mvn clean package -e
 
-docker build -t teenthofabud/nandi-foods-service-manager:0.1.4-RELEASE -f Dockerfile .
+docker build -t teenthofabud/nandi-foods-service-manager:${VERSION} -f Dockerfile .
 
-docker run -p 8080:8080 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:0.1.4-RELEASE
+docker run -p 8080:8080 -d -e PROFILE=default,staging --name nandi-foods-service-manager teenthofabud/nandi-foods-service-manager:${VERSION}
 
-docker image push teenthofabud/nandi-foods-service-manager:0.1.4-RELEASE
+docker image push teenthofabud/nandi-foods-service-manager:${VERSION}
 
-docker pull docker.io/teenthofabud/nandi-foods-service-manager:0.1.4-RELEASE
+docker pull docker.io/teenthofabud/nandi-foods-service-manager:${VERSION}
+
+git pull
+
+git tag ${VERSION} -a -m "Release version ${VERSION}"
+
+git push origin ${VERSION}
