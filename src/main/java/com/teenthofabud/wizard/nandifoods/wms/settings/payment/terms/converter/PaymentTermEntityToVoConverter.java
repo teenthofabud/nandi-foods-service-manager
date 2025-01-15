540ffdc28bd3355f28328ee3386e5e9083f8a550
shahad7 <shahadneeliyatt786@gmail.com>
2025-01-15 22:54:41 +0530

    missed annotation

--- src/main/java/com/teenthofabud/wizard/nandifoods/wms/settings/payment/terms/converter/PaymentTermEntityToVoConverter.java
+++ src/main/java/com/teenthofabud/wizard/nandifoods/wms/settings/payment/terms/converter/PaymentTermEntityToVoConverter.java
@@ -3,7 +3,9 @@ package com.teenthofabud.wizard.nandifoods.wms.settings.payment.terms.converter;
 import com.teenthofabud.wizard.nandifoods.wms.settings.payment.terms.entity.PaymentTermEntity;
 import com.teenthofabud.wizard.nandifoods.wms.settings.payment.terms.vo.PaymentTermVo;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PaymentTermEntityToVoConverter implements Converter<PaymentTermEntity, PaymentTermVo> {
     @Override
     public PaymentTermVo convert(PaymentTermEntity source) {
