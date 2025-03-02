74d8f84decca48c6edc925f7a8de1f0827cdff94
shahad7 <shahadneeliyatt786@gmail.com>
2025-03-02 17:22:15 +0530

    Deleted UnitClassCollectionItem contract

--- src/main/java/com/teenthofabud/wizard/nandifoods/wms/settings/unit/type/UnitClassCollectionItem.java
+++
@@ -1,7 +0,0 @@
-package com.teenthofabud.wizard.nandifoods.wms.settings.unit.type;
-
-public interface UnitClassCollectionItem<T> {
-
-    /** Returns the same JaVers @Id annotated property as an identifier for the list object*/
-    T getId();
-}
