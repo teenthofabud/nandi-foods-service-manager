afb1ed33673da6944fc1fd38e6677d8b6d312188
Arnab <87225675+arnabj2762@users.noreply.github.com>
2025-05-06 17:58:32 +0530

    Added comments UOMDtoV2toUOMEntityPatcher.java

--- src/main/java/com/teenthofabud/wizard/nandifoods/wms/settings/unit/uom/converter/UOMDtoV2toUOMEntityPatcher.java
+++ src/main/java/com/teenthofabud/wizard/nandifoods/wms/settings/unit/uom/converter/UOMDtoV2toUOMEntityPatcher.java
@@ -3,6 +3,13 @@ package com.teenthofabud.wizard.nandifoods.wms.settings.unit.uom.converter;
 import com.teenthofabud.wizard.nandifoods.wms.settings.unit.uom.dto.UOMDtoV2;
 import com.teenthofabud.wizard.nandifoods.wms.settings.unit.uom.entity.UOMEntity;
 
+/*
+ * Used when applying a JSON Patch to a DTO and selectively updating only specific fields
+ * on the entity, without overwriting the entire object. Helps maintain separation of
+ * concerns between patching logic and persistence logic.
+ * Follows SOLID principles by isolating patch logic (SRP) and allowing flexible extension (OCP).
+ */
+
 public interface UOMDtoV2toUOMEntityPatcher {
     public UOMEntity scalerPatcher(UOMDtoV2 dto, UOMEntity entity);
 }
